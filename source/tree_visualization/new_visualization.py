3ca5495b5964f5013435dce2265aff86ac9a6b12
Mateus <matbessa12@gmail.com>
2024-06-24 15:35:50 -0300

    colored edges

--- source/tree_visualization/new_visualization.py
+++ source/tree_visualization/new_visualization.py
@@ -7,9 +7,14 @@ from source.paths.path_reference import get_tree_pkl_path
 from source.raptor.tree_structures import Tree, Node
 from source.tree_visualization.visualization_utils import create_root_node, format_text_for_plot
 
-NODE_SIZE = 70
+NODE_SIZE = 35
+CANVAS_WIDTH = 1500
+CANVAS_HEIGHT = 800
+
 DEFAULT_NODE_COLOR = "#6175c1"
+DEFAULT_EDGE_COLOR = "rgb(210,210,210)"
 HIGHLIGHTED_NODE_COLOR = "red"
+HIGHLIGHTED_EDGE_COLOR = "red"
 
 
 class NewTreeVisualizer:
@@ -19,7 +24,8 @@ class NewTreeVisualizer:
         self.nodes = []
         self.edges = []
         self.labels = []
-        self.special_nodes = []
+        self.fig = go.Figure()
+        self.node_index_mapping = {}
 
     def add_node_to_graph(self, node: Node, parent_node_id: int = None):
         """
@@ -27,6 +33,7 @@ class NewTreeVisualizer:
         """
         node_id = len(self.nodes)
         self.nodes.append((node_id, node.index))
+        # self.node_index_mapping[node.index] = node_id
         self.labels.append(f"Node #{node.index}<br>{format_text_for_plot(node.text)}")
 
         if parent_node_id is not None:
@@ -47,18 +54,24 @@ class NewTreeVisualizer:
         except StopIteration:
             raise KeyError(f"Node with index {node_index} not found")
 
-    def highlight_nodes(self, trail: List[int]):
-        self.special_nodes = trail
+    def create_igraph_object(self):
+        g = Graph(directed=True)
 
-    def plot_tree(self):
+        for node_id, node_index in self.nodes:
+            name = f"{format_text_for_plot(self.labels[node_id])}"
+            self.node_index_mapping[node_id] = node_index
+            vertex_id = g.add_vertex(name=name).index
+
+        for edge in self.edges:
+            target, source = edge
+            g.add_edge(target, source)
+        return g
+
+    def plot_tree(self, special_nodes: List[int]):
         self.add_node_to_graph(self.root_node)
 
         # Create an igraph graph to use the Reingold-Tilford layout
-        g = Graph(directed=True)
-        for node_id, node_index in self.nodes:
-            g.add_vertex(name=f"{format_text_for_plot(self.labels[node_id])}")
-        for edge in self.edges:
-            g.add_edge(edge[0], edge[1])
+        g = self.create_igraph_object()
 
         # Get Reingold-Tilford layout
         layout = g.layout_reingold_tilford(root=[0])
@@ -66,54 +79,70 @@ class NewTreeVisualizer:
         # Calculate x and y values
         x_values = [layout[i][0] for i in range(len(g.vs))]
         max_y_value = max(layout[i][1] for i in range(len(g.vs)))
-        y_values = [max_y_value - layout[i][1] for i in range(len(g.vs))]  # Flip y-axis
+        y_values = [max_y_value - layout[i][1] for i in range(len(g.vs))]
 
-        colors = [HIGHLIGHTED_NODE_COLOR if node[1] in self.special_nodes else DEFAULT_NODE_COLOR for node in self.nodes]
-
-        marker_dict = dict(
-            symbol="circle",
-            size=25,
-            color=colors,
-            line=dict(color="black", width=1),
-        )
-
-        fig = go.Figure()
-
-        # Add edges
-        for edge in g.es:
-            x0, y0 = layout[edge.source]
-            x1, y1 = layout[edge.target]
-            y0 = max_y_value - y0  # Flip y-axis for edges
-            y1 = max_y_value - y1  # Flip y-axis for edges
-            fig.add_trace(go.Scatter(
-                x=[x0, x1, None], y=[y0, y1, None],
-                mode='lines',
-                line=dict(width=1, color='gray')
-            ))
-
-        fig.add_trace(go.Scatter(
-            x=x_values, y=y_values,
-            mode='markers+text',
-            marker=marker_dict,
-            text=[str(index) for _, index in self.nodes],
-            hoverinfo='text',
-            hovertext=[vertex["name"] for vertex in g.vs],
-            textposition="top center"
-        ))
+        self.plot_edges(layout, max_y_value, g, special_nodes)
+        self.plot_nodes(x_values, y_values, g, special_nodes)
 
         # Update layout
-        fig.update_layout(
+        self.fig.update_layout(
             showlegend=False,
             xaxis=dict(showgrid=False, zeroline=False),
             yaxis=dict(showgrid=False, zeroline=False),
             plot_bgcolor='white',
             autosize=False,
-            width=1200,
-            height=800,
+            width=CANVAS_WIDTH,
+            height=CANVAS_HEIGHT,
             margin=dict(l=40, r=40, b=40, t=40),
         )
 
-        fig.show()
+        self.fig.show()
+
+    def plot_nodes(self, x_values: List[float], y_values: List[float], igraph_graph: Graph, special_nodes: List[int]):
+        colors = [HIGHLIGHTED_NODE_COLOR if node[1] in special_nodes else DEFAULT_NODE_COLOR for node in
+                  self.nodes]
+
+        marker_dict = dict(
+            symbol="circle",
+            size=NODE_SIZE,
+            color=colors,
+            line=dict(color="black", width=1),
+        )
+
+        node_scatter = go.Scatter(
+            x=x_values, y=y_values,
+            mode='markers+text',
+            marker=marker_dict,
+            text=[str(index) for _, index in self.nodes],
+            hoverinfo='text',
+            hovertext=[vertex["name"] for vertex in igraph_graph.vs],
+            textposition="top center"
+        )
+
+        self.fig.add_trace(node_scatter)
+
+    def plot_edges(self, layout: List[List[float]], max_y_value: float, igraph_graph: Graph, special_nodes: List[int]):
+        all_edges = [(edge.source, edge.target) for edge in igraph_graph.es]
+        mapped_edges = [(self.node_index_mapping[source], self.node_index_mapping[target]) for source, target in
+                        all_edges]
+        special_edges = [(source, target) for source, target in mapped_edges
+                         if source in special_nodes and target in special_nodes]
+        reverse_map = {v: k for k, v in self.node_index_mapping.items()}
+        unmapped_edges = [(reverse_map[source], reverse_map[target]) for source, target in special_edges]
+
+        for edge in igraph_graph.es:
+            x0, y0 = layout[edge.source]
+            x1, y1 = layout[edge.target]
+            y0 = max_y_value - y0  # Flip y-axis for edges
+            y1 = max_y_value - y1  # Flip y-axis for edges
+
+            edge_color = HIGHLIGHTED_EDGE_COLOR if (edge.source, edge.target) in unmapped_edges else DEFAULT_EDGE_COLOR
+
+            self.fig.add_trace(go.Scatter(
+                x=[x0, x1, None], y=[y0, y1, None],
+                mode='lines',
+                line=dict(width=1, color=edge_color)
+            ))
 
 
 def main():
@@ -123,8 +152,7 @@ def main():
 
     trail = [-1, 37, 9]
     new_tree_visualizer = NewTreeVisualizer(tree_object)
-    new_tree_visualizer.highlight_nodes(trail)
-    new_tree_visualizer.plot_tree()
+    new_tree_visualizer.plot_tree(trail)
 
 
 if __name__ == '__main__':
